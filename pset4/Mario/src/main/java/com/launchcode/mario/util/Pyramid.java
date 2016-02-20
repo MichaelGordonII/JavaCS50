53876f1d4e080a6adf70bc8ede95a83ddc9b129d
Michael Gordon <ciscomike42@yahoo.com>
2016-02-20 16:17:45 -0600

    Added files via upload

---
+++ pset4/Mario/src/main/java/com/launchcode/mario/util/Pyramid.java
@@ -0,0 +1,42 @@
+package com.launchcode.mario.util;
+
+public class Pyramid
+{
+    int columns;
+    int rows;
+    public void setRows(int value)
+    {
+        rows = value;
+    }
+    public void setColumns(int value)
+    {
+        columns = value;
+    }
+    public Pyramid(){}
+    public Pyramid(int height)
+    {
+        rows = height;
+        columns = height + 1;
+    }
+    public java.lang.String toString()
+    {
+        StringBuilder result = new StringBuilder();
+        int blockIndex = 2;
+        int spaceIndex = 0;
+        for (int rowIndex = 0; rowIndex < rows; rowIndex++)
+        {
+            spaceIndex = columns - blockIndex;
+            for (int spaces = spaceIndex; spaces > 0; spaces--)
+            {
+                result.append(" ");
+            }
+            for (int block = blockIndex; block > 0; block--)
+            {
+                result.append("#");
+            }
+            result.append("\r\n");
+            blockIndex++;
+        }
+        return(result.toString());
+    }
+}
