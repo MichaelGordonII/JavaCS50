afbc82042103fe6f30f956985b6d6e9512087324
Michael Gordon <ciscomike42@yahoo.com>
2016-03-12 10:24:23 -0600

    Added files via upload

---
+++ pset5/Greedy/src/test/java/SimpleTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class SimpleTest
+{
+    @Test
+    public void testOneEqualsOne()
+    {
+        assertEquals(1,1);
+    }
+    @Test
+    public void testStringOneEqualsStringTwo()
+    {
+        assertEquals("1", "1");
+    }
+}
