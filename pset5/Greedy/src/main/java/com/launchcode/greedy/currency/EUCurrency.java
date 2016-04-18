b0a97ad3063be162d19d1779477d2e815deb5810
Michael Gordon <ciscomike42@yahoo.com>
2016-04-18 13:20:03 -0500

    Update EUCurrency.java

--- pset5/Greedy/src/main/java/com/launchcode/greedy/currency/EUCurrency.java
+++ pset5/Greedy/src/main/java/com/launchcode/greedy/currency/EUCurrency.java
@@ -5,18 +5,9 @@ import com.launchcode.greedy.CurrencyFactory;
 public class EUCurrency implements CurrencyFactory
 {
     private final int Coins[] = {200,100,50,20,10,5,2,1};
-    public int countCoinsInChange(int change)
+    public int[] getCoinValues()
     {
-        int coinCount = 0;
-        for(int coinIndex = 0; coinIndex < this.Coins.length; coinIndex++)
-        {
-            while(change >= this.Coins[coinIndex])
-            {
-                change -= this.Coins[coinIndex];
-                coinCount++;
-            }
-        }
-        return(coinCount);
+        return(Coins);
     }
 }
 
